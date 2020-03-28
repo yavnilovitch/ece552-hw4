fc7153e55f57263e92ae59aac1f9c2bd8f3a205b
Alexandra Yavnilovitch <yavnilovitch@rockhopper-07.cs.wisc.edu>
2020-03-28 14:27:22 -0500

    finalized part 2 tests

--- hw4/hw4_2/p2-1.asm
+++ hw4/hw4_2/p2-1.asm
@@ -2,11 +2,14 @@
 //		Abby Rechkin
 //		Alex Yavnilovitch
 // Simple test for branching
+// Showing benefit of 'predict taken' policy
 
 lbi r1, 8		// r1 = 8
 lbi r2, 8		// r2 = 8
 sub r2, r2, r1		// r2 = r2 - r1
 beqz r2, .label1	// if(r2 == 0) go to label1
 
+addi r2, r2, 6		// r2 = r2 + 6
+
 .label1:
-add r2, r1, r2		// r2 = r1 + r2 = 8
+halt
--- hw4/hw4_2/p2-2.asm
+++ hw4/hw4_2/p2-2.asm
@@ -2,11 +2,12 @@
 //              Abby Rechkin
 //              Alex Yavnilovitch
 // Simple test for branching: Showing why branch prediction is useful
+// Benefit of 'predict not taken' policy
 
 lbi r1, 8               // r1 = 8
 lbi r2, 8               // r2 = 8
 sub r2, r2, r1          // r2 = r2 - r1
-beqz r2, .label1        // if(r2 == 0) go to label1
+bnez r2, .label1        // if(r2 != 0) go to label1
 
 addi r1, r1, 5		// r1 = 13
 
